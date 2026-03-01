2bd28d374a7e1e677f119ec546fbaf1806cb1bf9
Flavien PERIER <flavien.perier@pm.me>
2026-03-01 14:34:12 +0100

    Refactor to multi-module

---
+++ domain/src/test/kotlin/io/flavien/demo/testCore/util/MailServerUtil.kt
@@ -0,0 +1,31 @@
+package io.flavien.demo.testCore.util
+
+import org.subethamail.wiser.Wiser
+import org.subethamail.wiser.WiserMessage
+import java.net.ServerSocket
+
+object MailServerUtil {
+
+    fun create(): Wiser {
+        val serverSocket = ServerSocket(0)
+        val port = serverSocket.localPort
+        serverSocket.close()
+
+        val smtp = Wiser.port(port)
+        smtp.start()
+
+        return smtp
+    }
+
+    fun getLastMail(smtp: Wiser, expectedNumberOfMails: Int): WiserMessage {
+        for (i in 1..40) {
+            Thread.sleep(50)
+            val messages = smtp.messages
+            if (messages.size == expectedNumberOfMails) {
+                return messages.last()
+            }
+        }
+
+        throw RuntimeException("Mail reception error")
+    }
+}
---
+++ front/src/main/typescript/vite.config.mts
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from "url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    target: "es2019",
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "api-generated": fileURLToPath(new URL("./src/api/generated", import.meta.url)),
+    },
+  },
+  server: {
+    base: "/vue-user.html",
+    proxy: {
+      "/api": {
+        changeOrigin: true,
+        target:  "http://127.0.0.1:8080/",
+      },
+    },
+  },
+});
